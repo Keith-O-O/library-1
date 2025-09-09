a95a437482addef9a6209bfa92b657a44a2f8d8c
Keith-O-O <keithodhiambo812@gmail.com>
2025-09-09 22:27:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("checkoutButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "You've successfully checked out a book!";
+});
